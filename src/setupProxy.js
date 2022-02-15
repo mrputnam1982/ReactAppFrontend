4cba6e45d37b94bd60222b438116b42e9606c952
Mike <mike.putnam@gmail.com>
2022-02-15 10:56:14 -0800

    Reverted addition of nginx configuration file, modified setupPRoxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
+  console.log("Using proxy API:", process.env.PROXY_API);
   app.use(
     createProxyMiddleware("/api/", { target: process.env.PROXY_API || "http://localhost:8080" })
   );
