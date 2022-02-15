31ebfb21dd5bff1abe51dd5b481054d0f86d8f4f
Mike <mike.putnam@gmail.com>
2022-02-15 11:14:54 -0800

    added server proxy info for registration and login

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,15 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  console.log("Using proxy API:", process.env.PROXY_API);
+  //console.log("Using proxy API:", process.env.PROXY_API);
   app.use(
     createProxyMiddleware("/api/", { target: process.env.PROXY_API || "http://localhost:8080" })
+
+  );
+  app.use(
+    createProxyMiddleware("/auth/register", { target: process.env.PROXY_LOGIN || "http://localhost:8080" })
+  );
+  app.use(
+    createProxyMiddleware("/auth/login", { target: process.env.PROXY_LOGIN || "http://localhost:8080" })
   );
 };
