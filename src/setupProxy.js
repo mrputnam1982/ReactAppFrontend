b957b5686f91bbeec37facf40736fdc2c4bcc35f
Mike <mike.putnam@gmail.com>
2022-02-15 11:46:59 -0800

    removed localhost option from proxy configuration

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,20 +4,20 @@ module.exports = function (app) {
   //console.log("Using proxy API:", process.env.PROXY_API);
   app.use(
     '/api/',
-    createProxyMiddleware({ target: process.env.PROXY_API || "http://localhost:8080",
+    createProxyMiddleware({ target: process.env.PROXY_API,
         changeOrigin: true,
     })
 
   );
   app.use(
     "/auth/register/",
-    createProxyMiddleware({ target: process.env.PROXY_LOGIN || "http://localhost:8080",
+    createProxyMiddleware({ target: process.env.PROXY_LOGIN,
         changeOrigin: true,
     })
   );
   app.use(
     "/auth/login/",
-    createProxyMiddleware({ target: process.env.PROXY_LOGIN || "http://localhost:8080",
+    createProxyMiddleware({ target: process.env.PROXY_LOGIN,
         changeOrigin: true,
     })
   );
